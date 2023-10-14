ed3a7eb33b433a64c73156f7e3bb637953ba05a1
Grace An Velayo <graceann151@gmail.com>
2023-10-14 23:02:33 +0800

    last commit

--- addMenu.php
+++ addMenu.php
@@ -1,51 +1,53 @@
-<html>
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>PHP Menu Input</title>
-    <!-- Add Bootstrap CSS -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.5.0/dist/sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.5.0/dist/sweetalert2.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
- 
-</head>
-<body>
-<nav class="navbar navbar-expand-lg bg-light ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Velayo POS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Manage</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Menu</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</nav>
-<div class="container">
-        <h1 class="text-center">Create Menu</h1>
-        <form action="user_input.php" method="post">
-            <div class="mb-3">
-                <label for="name" class="form-label">Menu Name</label>
-                <input type="text" class="form-control" id="name" name="name" required>
-            </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" id="description" name="description" rows="5" required></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-    <!-- Add Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoJtKh7z7lGz7fuP4F8nfdFvAOA6Gg/z6Y5J6XqqyGXYM2ntX5" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0v8FqFjcJ6pajs/rfdfs3SO+kD4Ck5BdPtF+to8xMp9Mvc9l84l+78+m696jLmfIjzj6t" crossorigin="anonymous"></script>
-</body>
-</html>
+
+<?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the menu name and description from the form
+    $menuName = $_POST["name"];
+    $menuDesc = $_POST["description"];
+
+    // Connect to the database
+    $dbHost = "localhost";
+    $dbUser = "root";
+    $dbPassword = "";
+    $dbName = "PointOfSale"; // Change to your actual database name
+
+    $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+    // Check the database connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Insert data into the database
+    $sql = "INSERT INTO ref_menu (menu_name, menu_desc) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $menuName, $menuDesc);
+
+    if ($stmt->execute()) {
+        // Data insertion was successful
+        echo "<script>
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Menu added to the database',
+                    icon: 'success',
+                }).then(function () {
+                    window.location.href = 'your_original_page.html'; // Redirect to your original page
+                });
+              </script>";
+    } else {
+        // Data insertion failed
+        echo "<script>
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'Failed to add menu to the database',
+                    icon: 'error',
+                });
+              </script>";
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
+}
+?>
